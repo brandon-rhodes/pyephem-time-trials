896c999b695d7b9244d2fce70953a7d29872453d
Brandon Rhodes <brandon@rhodesmill.org>
2012-09-02 14:09:26 -0400

    Added 3rd trial: Python version in C-Python

--- trial.sh
+++ trial.sh
@@ -16,12 +16,20 @@ then
 fi
 
 echo
-echo "PyEphem's libastro C-language back-end, running under Python:"
+echo "PyEphem's libastro C-language back-end, running under C Python:"
 echo
 
 PYTHONPATH=$THISDIR/pyephem/build/lib.linux-i686-2.7/ \
     python -m timeit -n 1 -r 1000 \
     -s 'import sat1' 'sat1.july_minutes()'
+
+echo
+echo "Pure-Python sgp4, running under C-Python:"
+echo
+
+PYTHONPATH=$THISDIR/sgp4 \
+    python -m timeit -n 1 -r 1000 \
+    -s 'import sat2' 'sat2.july_minutes()'
 
 echo
 echo "Pure-Python sgp4, running under PyPy:"
